e5fc191f05bc02cec6bdbaa399b7d0769a9d8ec7
some1less <muracks.max@gmail.com>
2025-03-18 13:25:01 +0100

    Implemented basic classes for entries

--- src/main/java/org/example/flashcards/mechanism/Entry.java
+++ src/main/java/org/example/flashcards/mechanism/Entry.java
@@ -1,4 +1,35 @@
 package org.example.flashcards.mechanism;
 
 public class Entry {
+
+    private String polish;
+    private String english;
+    private String german;
+
+    public Entry(String polish, String english, String german) {
+        this.polish = polish;
+        this.english = english;
+        this.german = german;
+    }
+
+    public String getPolish() {
+        return polish;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public String getGerman() {
+        return german;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "polish='" + polish + '\'' +
+                ", english='" + english + '\'' +
+                ", german='" + german + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/flashcards/mechanism/EntryRepository.java
+++ src/main/java/org/example/flashcards/mechanism/EntryRepository.java
@@ -1,4 +1,21 @@
 package org.example.flashcards.mechanism;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class EntryRepository {
+
+    private final List<Entry> entries = new ArrayList<>();
+
+    public void add (Entry entry) {
+        entries.add(entry);
+    }
+
+    public List<Entry> displayAll(){
+        return entries;
+    }
+
 }
